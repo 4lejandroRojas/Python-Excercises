d7c781426ae8bc17ee04c55bcd0c9fa607a4559c
Alejandro Rojas <4lejandr0rojas@protonmail.com>
2025-06-18 18:30:03 -0600

    .

---
+++ 07-Conditionals.py
@@ -0,0 +1,23 @@
+# Create a program that calcules the total note. Given the grades, absences & total classes.
+grade1 = float( input("Write the grade of the first test: "))
+grade2 = float( input("Write the grade of the second test: "))
+absences = int( input("Write the number of absences: "))
+totalClasses = int ( input("Write the number of classes: "))
+
+averageGrade = (grade1 + grade2) / 2
+attendance = (totalClasses - absences) / totalClasses
+
+# Print average grade and attendance
+print("Average grade: ", averageGrade)
+print("Attendance rate: ", str(round(attendance * 100, 2))+"%")
+
+
+if (averageGrade >= 6):
+    if(attendance >= 0.8):
+        print("The student has been approved.")
+    else:
+        print("The student has been failed due to an attence rate lower than 80%.")
+elif(attendance >= 0.8):
+    print("The student has been failed due to an average grade lower than 6.")
+else:
+    print("The student has been failed due to an average grade lower than 6 and an attence rate lower than 80%.")
